640208fc9f326ec185179aaf7d443ccad53780f9
jamnxdev <chovatiajaimin@gmail.com>
2026-02-01 23:19:51 +0530

    feat: add temp route link

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,26 @@
 import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
 
 export default function Header() {
   return (
     <header className="fixed block h-12 border-x-2 border-dashed top-0 z-10 left-1/2 -translate-x-1/2 max-w-4xl backdrop px-2 backdrop-blur-sm w-full">
       <nav className="flex items-center justify-between h-full">
-        <Link href={"/"}>/home</Link>
+        <div>
+          <Link href={"/"}>/home</Link>
+        </div>
+        <div className="space-x-2">
+          <Link href={"/projects"}>
+            <Button variant={"link"} className={"px-0 text-base"}>
+              projects
+            </Button>
+          </Link>
+          <Link href={"/blogs"}>
+            <Button variant={"link"} className={"px-0 text-base"}>
+              blogs
+            </Button>
+          </Link>
+        </div>
       </nav>
     </header>
   );
