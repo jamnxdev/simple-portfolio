ef4da1cae77c6ae369877f853a99abb7dbc24f6b
jamnxdev <chovatiajaimin@gmail.com>
2026-02-01 23:18:44 +0530

    style: x border not visible on small screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
       >
-        <main className="h-full flex flex-col border-dashed border-x-2 max-w-4xl mx-auto pt-12">
+        <main className="h-full flex flex-col border-none md:border-dashed md:border-x-2 max-w-4xl mx-auto pt-12">
           <Header />
           {children}
         </main>
