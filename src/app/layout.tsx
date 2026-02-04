ee97731f7bfd6f35256714ec5173c5d6f392a8b6
jamnxdev <chovatiajaimin@gmail.com>
2026-02-04 21:38:06 +0530

    style: removed full height body and html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable} suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
