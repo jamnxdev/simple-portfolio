39038eef8b03ffc27cb7b33a081b83ee57b5f571
jamnxdev <chovatiajaimin@gmail.com>
2026-02-11 23:13:54 +0530

    fix: typo in videoSRC value of projects

--- src/config/content/projects.ts
+++ src/config/content/projects.ts
@@ -19,7 +19,7 @@ export const PROJECTS: Project[] = [
       "DDoSim is an interactive platform for simulating and visualizing Distributed Denial-of-Service (DDoS) attacks in real time across the globe.",
     imgSRC:
       "https://raw.githubusercontent.com/DDoSimLab/Site/refs/heads/main/public/og-image.jpg",
-    videoSRC: "https://daydonezo.vercel.app",
+    videoSRC: "https://ddosim.vercel.app",
     status: "online",
     liveURL: "https://ddosim.live",
     backupLiveURL: "https://ddosim.vercel.app",
@@ -41,7 +41,7 @@ export const PROJECTS: Project[] = [
       "Digital journaling platform where you can document your daily wins and accomplishments.",
     imgSRC:
       "https://raw.githubusercontent.com/Jaimin25/DayDonezo/7e87aeb92b5633fc6e833ec40296849dfcd6fbce/public/Link%20Preview%20-%20DayDonezo.png",
-    videoSRC: "/demo-video/daydonezo-demo.mp4",
+    videoSRC: "/demo-videos/daydonezo-demo.mp4",
     status: "online",
     liveURL: "https://daydonezo.vercel.app",
     githubURL: "",
@@ -63,7 +63,7 @@ export const PROJECTS: Project[] = [
       "ConvoNest is a realtime web application for chatting, where you can add friends and chat with them individually or in a group",
     imgSRC:
       "https://raw.githubusercontent.com/Jaimin25/ConvoNest/refs/heads/main/public/Link%20Preview%20-%20ConvoNest.png",
-    videoSRC: "/demo-video/convonest-demo.mp4",
+    videoSRC: "/demo-videos/convonest-demo.mp4",
     status: "online",
     liveURL: "https://convonest.vercel.app",
     githubURL: "",
@@ -82,7 +82,7 @@ export const PROJECTS: Project[] = [
     description:
       "An online marketplace enabling real-time auctions and bidding, driving higher user engagement",
     imgSRC: "https://i.postimg.cc/3NMw7smq/Link-Preview-Claimit.png",
-    videoSRC: "/demo-video/claimit-demo.mp4",
+    videoSRC: "/demo-videos/claimit-demo.mp4",
     status: "degraded",
     liveURL: "https://claimit.vercel.app",
     githubURL: "",
@@ -103,7 +103,7 @@ export const PROJECTS: Project[] = [
     description: "A crowdfunding platform",
     imgSRC:
       "https://raw.githubusercontent.com/Jaimin25/Sahyog/fecf91be059173c47e7b743863b33b2472a2cfe4/public/Link%20Preview%20-%20Sahyog.png",
-    videoSRC: "/demo-video/sahyog-demo.mp4",
+    videoSRC: "/demo-videos/sahyog-demo.mp4",
     status: "online",
     liveURL: "https://sahyogweb.vercel.app",
     githubURL: "",
@@ -124,7 +124,7 @@ export const PROJECTS: Project[] = [
       "A fun project to explore great individuals who revolutionized the world with their skills and knowledge, shaping humanity's progress through groundbreaking contributions and innovations.",
     imgSRC:
       "https://raw.githubusercontent.com/Jaimin25/GreatMinds/1ae55c6fda15d3e98fc82467f2de18e04992ffc2/public/Images/Link%20Preview.png",
-    videoSRC: "/demo-video/grtminds-demo.mp4",
+    videoSRC: "/demo-videos/grtminds-demo.mp4",
     status: "maintenance",
     liveURL: "https://grtminds.vercel.app",
     githubURL: "",
