70871c46c75dcfaf54e17b8b9ea03cf6f03a1dbf
jamnxdev <chovatiajaimin@gmail.com>
2026-02-14 22:00:17 +0530

    fix: side borders extend to the end of the content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="h-full flex flex-col border-dashed border-x-2 max-w-3xl mx-2 md:mx-auto pt-12">
+          <main className="min-h-screen flex flex-col border-dashed border-x-2 max-w-3xl mx-2 md:mx-auto pt-12">
             <Header />
             {children}
           </main>
