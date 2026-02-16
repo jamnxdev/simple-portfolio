efdf449f63daa1a9ba52d046ec44145152a135fd
jamnxdev <chovatiajaimin@gmail.com>
2026-02-16 22:07:07 +0530

    fix(GithubGraph): ui changes

--- src/components/home/github-section.tsx
+++ src/components/home/github-section.tsx
@@ -1,20 +1,20 @@
-import { Suspense } from "react";
+import { Suspense } from "react"
 
-import { getGitHubContributions } from "@/lib/github-contribution";
+import { getGitHubContributions } from "@/lib/github-contribution"
 
 import {
   GitHubContributionFallback,
   GitHubContributionGraph,
-} from "../gh-contribution-graph";
+} from "../gh-contribution-graph"
 
 export default function GithubSection() {
-  const contributions = getGitHubContributions();
+  const contributions = getGitHubContributions()
 
   return (
-    <div className="border-dashed m-2 border">
+    <div className="border-b-2 border-dashed p-2">
       <Suspense fallback={<GitHubContributionFallback />}>
         <GitHubContributionGraph contributions={contributions} />
       </Suspense>
     </div>
-  );
+  )
 }
